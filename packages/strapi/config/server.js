94dfb9d70739aa5b45b3c065c6f6d3018b387960
d-kawaguchi <45481012+d-kawaguchi@users.noreply.github.com>
2021-10-12 17:56:46 +0900

    chore: strapiインストール

---
+++ packages/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1e48debd768d8c012457a4f187ba468'),
+    },
+  },
+});
---
+++ packages/strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2f430602-f488-4f22-a417-09c94dcd93d4'
+};
