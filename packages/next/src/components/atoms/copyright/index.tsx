0f6711fc6cee5d55e946c35041ec65cb3368af6c
d-kawaguchi <45481012+d-kawaguchi@users.noreply.github.com>
2021-11-02 09:43:41 +0900

    chore: linter系セットアップ

---
+++ packages/next/src/components/atoms/copyright/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+import styled from "styled-components";
+
+type Props = {
+  text: string
+}
+
+export const Copyright: FC<Props> = ({ text }) => <Paragraph>&copy; {text}</Paragraph>
+
+const Paragraph = styled.p`
+  color: palevioletred;
+  font-size: 1.5em;
+  text-align: center;
+`;
